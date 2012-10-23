1633fb72e0d1af9f4beef75bdb1c1e3287f95625
Tim Gornik <tim.gornik@duke.edu>
2012-10-22 23:03:08 -0400

    changes to elevator

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
 
-public class Elevator 
+
+public class Elevator implements Runnable
 {
+	private ArrayList<Person> listOfPeople = new ArrayList<Person>();
+	
+	private boolean doorOpened = false;
+	
 	public void VisitFloor()
 	{
 		
@@ -8,11 +14,34 @@ public class Elevator
 	
 	public void OpenDoors()
 	{
-		
+		doorOpened = true;
 	}
 	
 	public void CloseDoors()
 	{
+		doorOpened = false;
+	}
+	
+	public void Enter()
+
+	{
+	
+	}
+	
+	public void Exit()
+	{
+		
+	}
+	
+	public void RequestFloor()
+	{
+		
+	}
+	
+	
+	public void run()
+	{
+		
 		
 	}
 }
