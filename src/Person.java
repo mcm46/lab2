3eb426e7b5c810fbac5ea7eff69a7ea4a5b72d45
Chris Muto <mcm46@duke.edu>
2012-10-22 23:00:54 -0400

    Added person class

---
+++ src/Person.java
@@ -0,0 +1,33 @@
+
+public class Person
+{
+	private int currentFloor;
+	private int nextFloor;
+	
+	public Person(int currentFloor,int nextFloor)
+	{
+		this.currentFloor=currentFloor;
+		this.nextFloor=nextFloor;
+	}
+	
+	public void changeFloors()
+	{
+		
+	}
+	
+	private void enterElevator()
+	{
+		
+	}
+	
+	private void exitElevator()
+	{
+		
+	}
+	
+	private void requestFloor()
+	{
+		
+	}
+
+}
