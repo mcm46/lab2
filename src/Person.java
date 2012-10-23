dc10731721d04d8d0390dae26763d411a2d73e32
Chris Muto <mcm46@duke.edu>
2012-10-22 23:56:24 -0400

    person class

--- src/Person.java
+++ src/Person.java
@@ -3,29 +3,23 @@ public class Person
 {
 	private int currentFloor;
 	private int nextFloor;
+	private Building myBuilding;
+	private Elevator myElevator;
 	
-	public Person(int currentFloor,int nextFloor)
+	public Person(int currentFloor,int nextFloor, Building building,Elevator elevator)
 	{
 		this.currentFloor=currentFloor;
 		this.nextFloor=nextFloor;
+		myBuilding= building;
+		myElevator=elevator;
 	}
 	
-	public void changeFloors()
+	public void run()
 	{
 		
 	}
 	
-	private void enterElevator()
-	{
-		
-	}
-	
-	private void exitElevator()
-	{
-		
-	}
-	
-	private void requestFloor()
+	public void setNextFloor()
 	{
 		
 	}
